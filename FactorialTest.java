253f23335e8e0e2b5b5fc7252eb2e9cd47a21417
Valentina Loginova <106422622+vninkina@users.noreply.github.com>
2024-09-10 21:17:59 +0400

    Update FactorialTest.java

--- FactorialTest.java
+++ FactorialTest.java
@@ -15,9 +15,6 @@ public class FactorialTest {
                 () -> assertEquals(BigInteger.valueOf(6), Factorial.getFactorial(3), "Факториал числа 3 должен быть: 6"),
                 () -> assertEquals(BigInteger.valueOf(24), Factorial.getFactorial(4), "Факториал числа 4 должен быть: 24"),
                 () -> assertEquals(BigInteger.valueOf(120), Factorial.getFactorial(5), "Факториал числа 5 должен быть: 120"),
-                () -> assertEquals(BigInteger.valueOf(720), Factorial.getFactorial(6), "Факториал числа 6 должен быть: 720"),
-                () -> assertEquals(BigInteger.valueOf(5040), Factorial.getFactorial(7), "Факториал числа 7 должен быть: 5040"),
-                () -> assertEquals(BigInteger.valueOf(40320), Factorial.getFactorial(8), "Факториал числа 8 должен быть: 40320"),
                 () -> assertThrows(IllegalArgumentException.class, () -> Factorial.getFactorial(-1), "Должно быть выброшено исключение IllegalArgumentException для отрицательных чисел"));
     }
 }
